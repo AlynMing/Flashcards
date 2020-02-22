71fe06a09abfb81f531c55e03121568454160d56
Regina-Bass <reginabassvdl@gmail.com>
2020-02-21 20:44:53 -0500

    Flashcards and optional questions created
    Signed-off-by: Regina-Bass <reginabassvdl@gmail.com>

--- app/src/main/java/com/example/flashcards/MainActivity.java
+++ app/src/main/java/com/example/flashcards/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.flashcards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TextView text = (TextView) findViewById(R.id.option1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        findViewById(R.id.option1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option1).setBackgroundColor(Color.RED);
+                //text.setTextColor(Color.WHITE);
+            }
+        });
+
+        findViewById(R.id.option2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option2).setBackgroundColor(Color.RED);
+            }
+        });
+
+        findViewById(R.id.option3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option3).setBackgroundColor(Color.RED);
+            }
+        });
+
+        findViewById(R.id.option4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option4).setBackgroundColor(Color.GREEN);
+            }
+        });
+
     }
+
 }
