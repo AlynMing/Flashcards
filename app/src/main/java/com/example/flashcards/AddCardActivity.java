8f179253f74e7bcb8e679ae4030a85d10a9c2c7e
Regina Bass <reginabassvdl@gmail.com>
2020-03-07 02:32:12 -0500

    cancel button placed and works

--- app/src/main/java/com/example/flashcards/AddCardActivity.java
+++ app/src/main/java/com/example/flashcards/AddCardActivity.java
@@ -2,7 +2,9 @@ package com.example.flashcards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class AddCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+
+
+
+       findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+
+                AddCardActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
     }
 }
