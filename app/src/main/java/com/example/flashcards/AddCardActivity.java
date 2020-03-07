26c879a81e20c568d6717ce51e1967dce2da70d4
Regina Bass <reginabassvdl@gmail.com>
2020-03-07 04:14:25 -0500

    Toast Widget added

--- app/src/main/java/com/example/flashcards/AddCardActivity.java
+++ app/src/main/java/com/example/flashcards/AddCardActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -20,6 +22,30 @@ public class AddCardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+
+        findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if ((((EditText) findViewById(R.id.editQuestion)).getText().toString().isEmpty() ||
+                        ((EditText) findViewById(R.id.editAnswer)).getText().toString().isEmpty()
+                )) {
+                    Toast.makeText(getApplicationContext(), "Please Fill All Fields", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    String question = ((EditText) findViewById(R.id.editQuestion)).getText().toString();
+                    String answer = ((EditText) findViewById(R.id.editAnswer)).getText().toString();
+
+                    Intent data = new Intent(); // data goes in here
+                    data.putExtra("string1", question); // key 'question' is added into the intent
+                    data.putExtra("string2", answer); // key 'answer' is added into the intent
+                    setResult(RESULT_OK, data); // set result code and bundle data for response
+                    finish(); // closes this activity and pass data to the original activity that launched this activity
+                }
             }
         });
 
